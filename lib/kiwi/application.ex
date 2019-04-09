fe7a2bcb897caa3e26888dfd935343ae3b6afec2
Marius <marius@twostairs.com>
2019-04-10 01:29:07 +0200

    Adjusted application.ex

--- lib/kiwi/application.ex
+++ lib/kiwi/application.ex
@@ -1,31 +1,26 @@
 defmodule Kiwi.Application do
-    # See https://hexdocs.pm/elixir/Application.html
-    # for more information on OTP Applications
-    @moduledoc false
-
     @target Mix.target()
 
     use Application
 
     def start(_type, _args) do
-        # See https://hexdocs.pm/elixir/Supervisor.html
-        # for other strategies and supported options
+        Kiwi.Collection.Setting.init()
+
         opts = [strategy: :one_for_one, name: Kiwi.Supervisor]
         Supervisor.start_link(children(@target), opts)
     end
 
-    # List all child processes to be supervised
     def children(:host) do
         [
-            # Starts a worker by calling: Kiwi.Worker.start_link(arg)
-            # {Kiwi.Worker, arg},
+            {Kiwi.Keyboard, %{}},
+            Kiwi.Server
         ]
     end
 
     def children(_target) do
         [
-            # Starts a worker by calling: Kiwi.Worker.start_link(arg)
-            # {Kiwi.Worker, arg},
+            {Kiwi.Keyboard, %{}},
+            Kiwi.Server
         ]
     end
 end
