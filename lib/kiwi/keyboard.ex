98c2773c37c1006d8690815b36d93f9c8a080f0e
Marius <marius@twostairs.com>
2019-04-11 01:22:11 +0200

    Extended Kiwi.Keyboard

--- lib/kiwi/keyboard.ex
+++ lib/kiwi/keyboard.ex
@@ -3,23 +3,25 @@ defmodule Kiwi.Keyboard do
     use GenServer
 
     def keys(), do: %{
-        key_1_in_row_1: %{pin: 20},
-        key_2_in_row_1: %{pin: 16},
-        key_3_in_row_1: %{pin: 26},
-        key_1_in_row_2: %{pin: 6},
-        key_2_in_row_2: %{pin: 12},
-        key_3_in_row_2: %{pin: 13},
-        key_1_in_row_3: %{pin: 22},
-        key_2_in_row_3: %{pin: 24},
-        key_3_in_row_3: %{pin: 5},
-        key_1_in_row_4: %{pin: 17},
-        key_2_in_row_4: %{pin: 27},
-        key_3_in_row_4: %{pin: 23}
+        key_1_in_row_1: %{pin: 20, led: 1},
+        key_2_in_row_1: %{pin: 16, led: 5},
+        key_3_in_row_1: %{pin: 26, led: 9},
+        key_1_in_row_2: %{pin: 6,  led: 2},
+        key_2_in_row_2: %{pin: 12, led: 6},
+        key_3_in_row_2: %{pin: 13, led: 10},
+        key_1_in_row_3: %{pin: 22, led: 3},
+        key_2_in_row_3: %{pin: 24, led: 7},
+        key_3_in_row_3: %{pin: 5,  led: 11},
+        key_1_in_row_4: %{pin: 17, led: 4},
+        key_2_in_row_4: %{pin: 27, led: 8},
+        key_3_in_row_4: %{pin: 23, led: 12}
     }
 
-    def all_pins(), do: Enum.map(keys(), fn {_key_name, key_map} -> key_map |> Map.get(:pin) end)
+    def all_pins!(), do: Enum.map(keys(), fn {_key_name, key_map} -> key_map |> Map.get(:pin) end)
 
-    def key_name_from_pin(pin) when is_number(pin), do: (for {key_name, key_map} <- keys(), Map.get(key_map, :pin) == pin, do: key_name) |> Enum.at(0)
+    def key_name_from_pin!(pin) when is_number(pin), do: (for {key_name, key_map} <- keys(), Map.get(key_map, :pin) == pin, do: key_name) |> Enum.at(0)
+
+    def led_id_from_key_name!(key_name) when is_atom(key_name), do: Kiwi.Keyboard.keys() |> Map.get(key_name) |> Map.get(:led)
 
     def start_link(args) do
         GenServer.start_link __MODULE__, args, name: __MODULE__
@@ -28,7 +30,7 @@ defmodule Kiwi.Keyboard do
     def init(_state) do
         Logger.debug "Opening GPIOs ..."
 
-        gpios = for pin <- all_pins() do
+        gpios = for pin <- all_pins!() do
             with \
                 {:ok, gpio} <- Circuits.GPIO.open(pin, :input),
                 :ok <- Circuits.GPIO.set_interrupts(gpio, :both),
@@ -40,7 +42,12 @@ defmodule Kiwi.Keyboard do
 
         Logger.debug "Opened GPIOs!"
 
-        {:ok, %{gpios: gpios}}
+        Logger.debug "Initializing LEDs ..."
+        {:ok, ledarray} = Kiwi.LedArray.init()
+        ledarray |> Kiwi.LedArray.display()
+        Logger.debug "Initialized LEDs!"
+
+        {:ok, %{gpios: gpios, ledarray: ledarray}}
     end
 
     def state_increment_value(value) when is_number(value) do
@@ -59,10 +66,10 @@ defmodule Kiwi.Keyboard do
         new_state = case msg do
             {:circuits_gpio, pin, _, 0} ->
                 Logger.debug "Keydown for pin #{inspect pin}"
-                key_event(pin, "down", state)
+                key_event(pin, :keydown, state)
             {:circuits_gpio, pin, _, 1} ->
                 Logger.debug "Keyup for pin #{inspect pin}"
-                key_event(pin, "up", state)
+                key_event(pin, :keyup, state)
             other ->
                 Logger.debug "Retrieved other message: #{inspect other}"
                 state
@@ -72,27 +79,28 @@ defmodule Kiwi.Keyboard do
     end
 
     def key_event(pin, event, state) do
-        key_name = key_name_from_pin(pin)
+        key_name = key_name_from_pin!(pin)
         {_, new_state} = state |> Map.get_and_update(key_name, fn current_value -> {current_value, state_increment_value(current_value)} end)
 
         case new_state |> Map.get(key_name) do
-            received_messages when received_messages > 2 -> key_action(key_name, event)
-            _ -> Logger.debug "Ignoring event for now."
+            received_messages when received_messages > 2 ->
+                key_action(key_name, event, new_state)
+            _ ->
+                Logger.debug "Ignoring event for now."
+                new_state
         end
-
-        new_state
     end
 
-    def key_action(key_name, "down") do
+    def key_action(key_name, event, state) do
         key_name_str = Atom.to_string(key_name)
         case Kiwi.Collection.Setting.findOne(key_name_str) do
-            {:ok, key_setting} -> Kiwi.Action.run(Jason.decode!(key_setting.value))
-            _ -> Logger.info "No key action found for #{key_name_str}!"
+            {:ok, key_setting} ->
+                action_map = Kiwi.Helpers.Settings.get_params_from_id_value(key_setting)
+                Logger.debug("Got action map: #{inspect action_map}")
+                Kiwi.Action.run(action_map, event, state)
+            _ ->
+                Logger.info "No key action defined for #{key_name_str}!"
+                state
         end
-    end
-
-    def key_action(key_name, "up") do
-        key_name_str = Atom.to_string(key_name)
-        Logger.info "Keyup for #{key_name_str}!"
     end
 end
