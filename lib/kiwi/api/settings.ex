f089e9f367e9d167565ee53c6bb6f7cdf8e1354e
Marius <marius@twostairs.com>
2019-04-13 01:20:29 +0200

    Implemented /settings/animations/:id

--- lib/kiwi/api/settings.ex
+++ lib/kiwi/api/settings.ex
@@ -41,5 +41,32 @@ defmodule Kiwi.Api.Settings do
                 end
             end
         end
+        namespace :animations do
+            route_param :id, type: String do
+                get do
+                    case Kiwi.Collection.Setting.findOne(params[:id]) do
+                        {:ok, found_model} -> conn |> resp({:ok, Kiwi.Helpers.Settings.get_params_from_id_value(found_model)})
+                        other -> conn |> resp({:error, other})
+                    end
+                end
+
+                desc "Update Animation Setting"
+                params do
+                    requires :object, type: Map do
+                        requires :frames, type: List do
+                            use :params_frame
+                        end
+                    end
+                end
+                post do
+                    case struct(Kiwi.Collection.Setting, Kiwi.Helpers.Settings.get_id_value_from_params(params)) |> Kiwi.Collection.Setting.upsert do
+                        :ok -> conn |> resp({:ok, nil})
+                        other ->
+                            Logger.error("#{inspect other}")
+                            conn |> resp({:error, nil})
+                    end
+                end
+            end
+        end
     end
 end
